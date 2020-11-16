40b23e49d3079c618ae0e248a818fbd9742a4676
VitaliyDorofechyk <72878947+VitaliyDorofechyk@users.noreply.github.com>
2020-11-16 23:20:59 +0300

    Update task6.js

--- task/task6.js
+++ task/task6.js
@@ -1,15 +1,15 @@
-//const minus = (a, b) => {
-//    return a - b;
-//}
-//const plus = (a, b) => {
-//    return a + b;
-//}
-//const divide = (a, b) => {
-//    return a / b;
-//}
-//const multiply = (a, b) => {
-//    return a * b;
-//}
+const minus = (a, b) => {
+    return a - b;
+}
+const plus = (a, b) => {
+    return Number(a + b);
+}
+const divide = (a, b) => {
+    return a / b;
+}
+const multiply = (a, b) => {
+    return a * b;
+}
 //let a = prompt('Введите занчение a', 2);
 //let c = prompt('Введите знак что будем делать с a"+,-,*,/"', '+')
 //let b = prompt('Введите занчение b', 3);
@@ -18,20 +18,20 @@
 let doOperation = (a, c, b) => {
     switch(c) {
         case '+':
-            return (a + b);
+            return plus(a, b);
             break;
         case '-':
-            return (a - b);
+            return minus(a, b);
             break;
         case '/':
-            return (a / b);
+            return divide(a, b);
             break;
         case '*':
-            return multiply(a * b);
+            return multiply(a, b);
             break;
     }
 }
 let a = prompt('Введите занчение a', 2);
 let c = prompt('Введите знак что будем делать с a"+,-,*,/"', '+')
 let b = prompt('Введите занчение b', 3);
-alert(`Ответ: ${doOperation}`);
+alert(`Ответ: ${doOperation(a, b, c) }`);
