287beead1565503ba28f3c65e115f2d4dfa73070
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-23 00:27:36 +0300

    changed task/task2/task2.js

---
+++ task/task2/task2.js
@@ -0,0 +1,25 @@
+const arr2 = ['hi', 'my', 'frend'];
+for( let i = 0; i < arr2.length; i++) {
+    for( let j = 0; j < arr2[i].length; j++) {
+        console.log(arr2[i][j].split(''));
+    } 
+};
+// Не понял задание до конца разбираться надо
+// const arr3 = [];
+// for( let i = 0; i < arr3.length; i++) {
+//     for( let j = 0; j < arr3[i].length; j++) {
+//         console.log(arr3[i][j].split(''));
+//     } 
+// };
+
+
+
+// const arr = ['hi', 'my', 'frend'];
+// for( i = 0; i < arr.length; i++) {
+//     for( j = 0; j < arr[i].length; j++) {
+//         return (`${i}:`, arr[i][j]);
+//     }
+// };
+
+
+
