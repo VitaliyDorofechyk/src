454a5a1d4dce9f30166a61e2dbc4e0e2162bc6cb
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-23 22:52:47 +0300

    change task/task2/task2.js and change index.html

--- task/task2/task2.js
+++ task/task2/task2.js
@@ -1,30 +1,7 @@
-const arr2 = ['hi', 'my', 'frend', 'Bob'];
-for( let i = 0; i < arr2.length; i++) {
-    for( let j = 0; j < arr2[i].length; j++) {
-        console.log(arr2[i].length);
-    } 
+const task2 = (arr) => {
+    console.log(arr.map(item => item.length));
 };
-
-
-
-
-
-
-
-// const arr2 = ['hi', 'my', 'frend'];
-// for( let i = 0; i < arr2.length; i++) {
-//     for( let j = 0; j < arr2[i].length; j++) {
-//         console.log(arr2[i][j].split(''));
-//     } 
-// };
-// Не понял задание до конца разбираться надо
-// const arr3 = [];
-// for( let i = 0; i < arr3.length; i++) {
-//     for( let j = 0; j < arr3[i].length; j++) {
-//         console.log(arr3[i][j].split(''));
-//     } 
-// };
-
+task2(['Знать', 'методы', 'массива','-', 'быть', 'крутым', 'разработчиком']);
 
 
 
