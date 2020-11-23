96f77ecddb22ed44d4bfbd7b4e9969b8f72ae2d1
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-24 00:46:06 +0300

    index.html change and task2.js add task3/js

--- task/task2/task2.js
+++ task/task2/task2.js
@@ -2,6 +2,3 @@ const task2 = (arr) => {
     console.log(arr.map(item => item.length));
 };
 task2(['Знать', 'методы', 'массива','-', 'быть', 'крутым', 'разработчиком']);
-
-
-
---
+++ task/task2/task3.js
@@ -0,0 +1,22 @@
+// const task3 = (arr3) => {
+//     const arr4 = [].concat(arr3);
+//     for( i = 0; i < arr3.length; i++) {
+//         if ( arr3[i] > arr3[i + 1] ) {
+//             arr3[i] = arr4;
+//         }
+//         else if ( arr3[i + 1] > arr3[i] ) {
+//             arr3[i + 1] = arr4;
+//         }
+//         else if ( arr3[i] == arr3[i + 1] ) {
+//             arr3[i] = arr4;
+//         }
+//     }
+// return arr4;
+// };
+// task3(['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9]);
+// console.log(arr4);
+
+// const arrTask3 = ['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9];
+// arrTask3.sort();
+// arrTask3.reverse();
+// console.log(arrTask3);
