c7b5fd1ba506f75a5e012766649bff89a99d7430
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-24 20:25:43 +0300

    add task3.js

--- task/task2/task3.js
+++ task/task2/task3.js
@@ -1,22 +1,17 @@
-// const task3 = (arr3) => {
-//     const arr4 = [].concat(arr3);
-//     for( i = 0; i < arr3.length; i++) {
-//         if ( arr3[i] > arr3[i + 1] ) {
-//             arr3[i] = arr4;
-//         }
-//         else if ( arr3[i + 1] > arr3[i] ) {
-//             arr3[i + 1] = arr4;
-//         }
-//         else if ( arr3[i] == arr3[i + 1] ) {
-//             arr3[i] = arr4;
-//         }
-//     }
-// return arr4;
-// };
-// task3(['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9]);
-// console.log(arr4);
+const task3 = (arr3) => {
+    arr3.forEach(element => {
+        if (typeof element === 'string') {
+            document.write(`${element} - string value </br>`);
+        }
+        else {
+            document.write(`${element} - number value </br>`);
+        }
+    });
+};
+task3(['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9]);
 
-// const arrTask3 = ['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9];
-// arrTask3.sort();
-// arrTask3.reverse();
-// console.log(arrTask3);
+
+const arrTask3 = ['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9];
+arrTask3.sort();
+arrTask3.reverse();
+console.log(arrTask3);
