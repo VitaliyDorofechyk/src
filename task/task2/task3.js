39a4e1265b2596f3f09841920db74912440ae04c
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-24 20:25:57 +0300

    add task3.js

--- task/task2/task3.js
+++ task/task2/task3.js
@@ -9,9 +9,11 @@ const task3 = (arr3) => {
     });
 };
 task3(['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9]);
+// если по условия переписал цикл for
 
 
 const arrTask3 = ['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9];
 arrTask3.sort();
 arrTask3.reverse();
 console.log(arrTask3);
+//а может так надо было....
