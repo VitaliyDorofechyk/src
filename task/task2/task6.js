5f522d97aeb7a3f002f26e2941d63c4bfe93f286
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-25 21:25:03 +0300

    changeindex.html and task6.js

--- task/task2/task6.js
+++ task/task2/task6.js
@@ -1,17 +1,21 @@
-const arr6 = [];
-const task5 = (message) => {
-    arr6.map((element, index)  => {
-        if ( typeof element === 'number') {
-            console.log(arr6[index].push(element));
-        }
-        else { alert('Это не число')};
-    });
-}; 
-// let num = () => { 
-//     let message = () =>{
-//     return prompt('Введите число', 2);
-//     }
-//     }    
-//     let message = Number(message);
-// };
-//     task5(message);
+
+const task6 = () => {
+    let arr6 = [];
+    let i = 0;
+    do { let num = parseFloat(prompt('я посчитаю любую сумму чисел которую ты введешь, чтоб узнать ответ введи 15', 2));
+    if ( num == 15) {
+        console.log(arr6);
+        let result = arr6.reduce((sum, current) => sum + Number(current),0);
+        console.log(`Сумма всех введеных чисел равна ${result}`);
+        break;
+    }
+    else if ( typeof num == 'number') {
+        arr6.push(`${num}`);
+    }
+    else {
+      alert('Это не число');
+    }
+    i++;
+    } while (true);
+};
+task6();
