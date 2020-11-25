ff9ef647cc7a42880871dba4bf20733c1c6c9606
vitaliy_dorofechyk <vitalik8754296@mail.ru>
2020-11-25 21:41:52 +0300

    change index.html and task3.js

--- task/task2/task3.js
+++ task/task2/task3.js
@@ -1,19 +1,19 @@
+const arrTask_3 = [];
 const task3 = (arr3) => {
     arr3.forEach(element => {
         if (typeof element === 'string') {
-            document.write(`${element} - string value </br>`);
+            arrTask_3.push(`${element} - string value`);
         }
         else {
-            document.write(`${element} - number value </br>`);
+            arrTask_3.push(`${element} - number value`);
         }
     });
 };
 task3(['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9]);
-// если по условия переписал цикл for
+// если я правильно понял задание
+// должно быть так
 
-
-const arrTask3 = ['Anna', 12, 'Sam', 9, 'Kate', 10, 'Ron', 9];
+const arrTask3 = arrTask_3;
 arrTask3.sort();
 arrTask3.reverse();
 console.log(arrTask3);
-//а может так надо было....
